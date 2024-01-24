276e9dbe26a3dbb49c06a5e2ee2cee9e76a6a166
Matt Behrens <askedrelic@gmail.com>
2024-01-23 16:56:36 -0800

    more words

--- assets/intro.jsx
+++ assets/intro.jsx
@@ -11,12 +11,15 @@ const Intro = (props) => {
     }
     return (
         <div className={`z-10 absolute w-full h-full ${introVisible ? 'visible' : 'hidden'} print:hidden`} onClick={handleClick}>
-            <div className={`w-2/3 h-1/2 p-4 mt-8 m-auto bg-white border-4 border-teal-300 prose`}>
+            <div className={`w-2/3 p-4 mt-8 m-auto bg-white border-4 border-teal-300 prose`}>
                 <h1>{props.year} Printable Quarterly Calendar</h1>
-                <p>Click anywhere to see the calendar and print it.</p>
-                <p>It's recommend to print in Landscape orientation.</p>
-                <p>To view a different month or year, <a href={`/?month=1&year=${props.year +1}`}>feel free to adjust the "month" or "year" in the URL querystring.</a></p>
-                <p>Known bugs: Jan/Dec yearly cross-over week numbers don't work.</p>
+                <p>Click anywhere to see the calendar (or just print now, this notice isn't printed). And make sure you are printing in Landscape orientation.</p>
+                <p>Use the quarterly calendar to plan things in reality or highlight habit streaks!</p>
+                <p>To view a different month or year, <a href={`/?month=1&year=${props.year +1}`}>modify the "month" or "year" in the URL query string</a>.</p>
+                <p>Why print things? Why not? Starting at a real thing hanging on your wall can be effective for motivation.</p>
+                <br/>
+                <p>Known bugs: crossing Jan/Dec years week numbers don't work.</p>
+                <p>Source: <a href="https://github.com/askedrelic/quarterly-calendar">https://github.com/askedrelic/quarterly-calendar</a></p>
             </div>
         </div>
     );
